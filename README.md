b5252276957ce1ebadc8dd8843be5dee0d961bbe
Joey Yang <jyang@clearpathrobotics.com>
2022-04-28 13:05:50 -0400

    Initial package commit; convert ROS2 Image to OpenCV Mat

--- README.md
+++
@@ -1,2 +0,0 @@
-# ros2_lanebot
-Lane Following Robot Project for Clearpath Robotics' Hack Day 2022
---
+++ src/node.cpp
@@ -0,0 +1,62 @@
+#include <memory>
+
+#include "cv_bridge/cv_bridge.h"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/image_encodings.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/videoio.hpp"
+
+using std::placeholders::_1;
+
+static const std::string OPENCV_WINDOW = "Lanebot Window";
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "/color/preview/image", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      cv::namedWindow(OPENCV_WINDOW);
+    }
+
+    ~MinimalSubscriber()
+    {
+      cv::destroyWindow(OPENCV_WINDOW);
+    }
+
+  private:
+    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "Image");
+
+      cv_bridge::CvImagePtr cv_ptr;
+      try
+      {
+          cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+      }
+      catch (cv_bridge::Exception& e)
+      {
+          RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+          return;
+      }
+
+      cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+      cv::waitKey(3);
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
